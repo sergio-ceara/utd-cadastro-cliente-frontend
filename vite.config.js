6f4e44efe3fe27a66e2e1f1990d3bb4d439c7b0f
Sérgio Sousa <sergio_ceara@yahoo.com.br>
2024-09-08 16:40:36 -0300

    Atualização do arquivo vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Permite o acesso a partir de outros dispositivos na rede local
-    port: 5173, // Porta que o Vite usará
-  },  
 })
