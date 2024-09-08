5b6054e5b4a11b0326ba6991bd410c311eb11187
Sérgio Sousa <sergio_ceara@yahoo.com.br>
2024-09-08 10:05:30 -0300

    Atualização dos endereços

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import styles from './Home.module.css';
 import { FaUser } from 'react-icons/fa';
+//import { baseURL } from '../../services/api'; // Importar a URL base
 import { baseFront } from '../../services/api'; // Importar a URL base
 //<a href="http://localhost:5173/cliente/view" className={styles.link}>Cadastro de clientes</a>
 //<a href={`${baseFront}/cliente/view`} className={styles.link}>Cadastro de clientes</a>
--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
+
 //baseURL:"http://localhost:8080/api"
 //baseURL:"http://192.168.3.10:8080/api"
 //baseURL
 //const baseURL = "http://192.168.3.10:8080/api"; // URL base da API
-const baseFront = "http://192.168.3.10:5173"
+
+//const baseFront = "http://192.168.3.10:5173"
+//baseURL:"http://192.168.3.10:8080/api"
+const baseFront = "https://utd-cadastro-cliente-frontend.vercel.app/"
+
 const api = axios.create({
-    baseURL:"http://192.168.3.10:8080/api"
+    baseURL:"https://utdcadastro-clientebackend-production.up.railway.app/api"
 });
 export {baseFront};
 export default api;
